e56ca2ce1a4192f6c9f0bc2ed1b8d39449ac15cf
KMASAHIRO <k.masa1215@gmail.com>
2021-09-14 03:47:41 +0900

    change from original

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from setuptools import setup, find_packages
 
 REQUIRED = ['gym', 'numpy', 'pandas', 'ray[rllib]']
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='sumo-rl',
-    version='1.0',
+    version='2.0',
     packages=['sumo_rl',],
     install_requires=REQUIRED,
     author='LucasAlegre',
     author_email='lucasnale@gmail.com',
-    url='https://github.com/LucasAlegre/sumo-rl',
-    download_url='https://github.com/LucasAlegre/sumo-rl/archive/v1.0.tar.gz',
+    url='https://github.com/KMASAHIRO/sumo-rl',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     license="MIT",
