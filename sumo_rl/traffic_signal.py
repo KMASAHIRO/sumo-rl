f861f673a6848206ee0a4b6b3cb0b15978f7d608
KMASAHIRO <k.masa1215@gmail.com>
2021-12-09 05:34:49 +0900

    compute obeservation every step

--- sumo_rl/traffic_signal.py
+++ sumo_rl/traffic_signal.py
@@ -81,8 +81,8 @@ class TrafficSignal:
             new_phase *= 2
         
         if new_phase is None or self.phase == new_phase or self.time_since_last_phase_change < self.min_green + self.yellow_time:
-            self.green_phase = self.phase
-            traci.trafficlight.setPhase(self.id, self.green_phase)
+            #self.green_phase = self.phase
+            #traci.trafficlight.setPhase(self.id, self.green_phase)
             if self.time_since_last_phase_change < self.min_green + self.yellow_time:
                 self.next_action_time = self.env.sim_step + self.min_green + self.yellow_time - self.time_since_last_phase_change
             else:
